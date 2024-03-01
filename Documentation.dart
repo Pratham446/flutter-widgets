90bfb23721dabee7c83c8e39e66d0579098d025a
Pratham Darji <darjipratham99@gmail.com>
2024-03-01 09:55:45 +0530

    Document

---
+++ Documentation.dart
@@ -0,0 +1,38 @@
+// git init
+// git add
+//git status
+//git commit -m "enter commit statement"
+//git push origin
+//git status
+// rm -to delete file 
+
+/*
+extension========>
+
+advance new file 
+better comments
+code runner
+devcontainer
+flutter widgets snippet
+github pull request
+material icon theme
+*/
+
+/*
+
+Shortcuts======>
+alt shift down for repetation of code 
+ctrl L for highliting the line 
+ctrl alt N ==> for new file 
+ctrl p => to switch between files 
+ctrl shift p => Command Palet
+
+
+*/
+/*
+windows shortcutsN
+ctr shift tab => previous tab  
+window E for opening file explorer
+alt left arrow => gooing back to in tab 
+crtl e for searchbar 
+*/
